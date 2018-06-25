7364a7b044efa11ea5fa4bd737ede11abc2d5bd1
Marc Teale <marc.teale@openmarket.com>
2018-06-25 14:01:44 -0700

    Add ability to run from wget command piped to bash
    - README.md: Update with new command, correct spelling, improve
    formatting.
    - makeSaveDirs.sh: Functionality moved into retroTINK-setup.sh.
    - Make runcommand-onend.sh, runcommand-onstart.sh, and
    retroTINK-setup.sh executable.
    - retroTINK-setup.sh: Add ability to run from wget command piped to
    bash, other optimizations.

--- README.md
+++ README.md
@@ -15,16 +15,47 @@ Console/SSH into your fresh retropie install.
 
 Type/paste command (All one line)
 
-`sudo apt-get install wget git -y && cd /home/pi/ && git clone https://github.com/Vykyan/retroTINK-setup.git && cd retroTINK-setup && chmod +x ./retroTINK-setup.sh && sudo ./retroTINK-setup.sh`
+`wget https://raw.githubusercontent.com/marcteale/retroTINK-setup/master/retroTINK-setup.sh -O - | sudo bash`
 
 Follow Instructions. (Make sure to change emulationstation theme to "tft-retrotink" once back in Emulationstation after reboot!
 
 # Currently supported Libretro Systems:
- (Not emulator dependant, must be libretro/retroarch core)
+ (Not emulator dependent, must be libretro/retroarch core)
  (Ex: snes will catch any SNES libretro core, ie, lr-snes9x,lr-pocketsnes etc)
 
-atari2600,atari5200,atari7800,atarilynx,fba,fds,gamegear,gb,gba,gbc,genesis/megadrive,mame-libretro,mastersystem,msx,n64,neogeo,nes,ngp,ngpc,pc,pcengine,pcenginecd,psx,sega32x,segacd,snes,supergrafx,virtualboy,wonderswan,wonderswancolor
+- atari2600
+- atari5200
+- atari7800
+- atarilynx
+- fba
+- fds
+- gamegear
+- gb
+- gba
+- gbc
+- genesis/megadrive
+- mame-libretro
+- mastersystem
+- msx
+- n64
+- neogeo
+- nes
+- ngp
+- ngpc
+- pc
+- pcengine
+- pcenginecd
+- psx
+- sega32x
+- segacd
+- snes
+- supergrafx
+- virtualboy
+- wonderswan
+- wonderswancolor
 
 # Currently supported OTHER emulators/ports:
 
-advmame (relies of advmame inbuilt support for changing resolutions),doom,quake
+- advmame (relies of advmame inbuilt support for changing resolutions)
+- doom
+- quake
--- retroTINK-setup.sh
+++ retroTINK-setup.sh
@@ -1,164 +1,113 @@
 #!/bin/bash
-#
-# COMMAND TO RUN FROM PI SSH/TERMINAL;
-#
-# sudo apt-get install wget git -y && cd /home/pi/ && git clone https://github.com/Vykyan/retroTINK-setup.git && cd retroTINK-setup && chmod +x ./retroTINK-setup.sh && sudo ./retroTINK-setup.sh
-#
-# Reminder 
-# executable files = mode 755
-# plain text files = mode 644
-#
-# Remember to use on files = chown pi:pi
-#
-
-# Check for Root Access
 
 killall emulationstation
 
+# Exit on error
+set -o errexit
+TEMPDIR=$(mktemp -d)
+
+# Check for Root Access
 Check_Root (){
-DIALOG_ROOT=${DIALOG=dialog}
-me=`basename "$0"`
+    DIALOG_ROOT=${DIALOG=dialog}
 
-if [[ $EUID -ne 0 ]]; then
-   $DIALOG_ROOT --title  "This script must be run as root" --clear \
-   --msgbox "\n\nType:\n\nsudo ./$me" 11 40
-   exit 1
-fi
-}
-
-# Check for Internet Access
-Check_Internet () {
-DIALOG_INTERNET=${DIALOG=dialog}
-
-wget -q --tries=10 --timeout=20 --spider http://www.google.com
-if [[ $? -ne 0 ]]; then
-   $DIALOG_INTERNET --title  "No Internet Access!" --clear \
-   --msgbox "\n\nPlease connect the Raspberry Pi to the Internet, required to continue setting up RetroTINK..." 11 40
-   exit 1
-fi
+    if [[ $EUID -ne 0 ]]; then
+        $DIALOG_ROOT --title  "Insufficient privileges" --clear \
+        --msgbox "\n\nThis script must be run as root.\n\nUsage:\n  wget https://raw.githubusercontent.com/marcteale/retroTINK-setup/master/retroTINK-setup.sh -O - | sudo bash" 11 112
+        exit 1
+    fi
 }
 
 Continue_Install (){
-clear
-# Create Save Dirs
-chmod 755 ./makeSaveDirs.sh
-./makeSaveDirs.sh
-if [[ $? -ne 0 ]]; then
-   echo "Error: Could not create Save file Directories!"
-fi
-chown pi:pi -R /home/pi/RetroPie/savefiles
-chown pi:pi -R /home/pi/RetroPie/savestates
+    clear
+    # Create Save Dirs
+    SYSTEMS='arcade atari2600 atari5200 atari7800 atarilynx fds gamegear gb
+    gba gbc mastersystem megadrive msx n64 neogeo nes ngp ngpc pce-cd pcengine
+    ports/doom ports/quake psx sega32x segacd snes supergrafx virtualboy
+    wonderswan wonderswancolor'
 
+    for SYSTEM in $SYSTEMS; do
+        mkdir -p /home/pi/RetroPie/save{files,states}/$SYSTEM
+    done
+    chown pi:pi -R /home/pi/RetroPie/save{files,states}
 
-#  Copy systems config
-chmod 644 ./es_systems.cfg
-chown pi:pi ./es_systems.cfg
-cp ./es_systems.cfg /etc/emulationstation/
+    # Copy systems config
+    cp /etc/emulationstation/es_systems.cfg{,.retrotink}
+    cp $TEMPDIR/es_systems.cfg /etc/emulationstation/
 
-# Copy Runcommands
-cd opt-retropie-configs-all
-chmod 755 ./runcommand-onend.sh
-chown pi:pi ./runcommand-onend.sh
-chmod 755 ./runcommand-onstart.sh
-chown pi:pi ./runcommand-onstart.sh
-cp ./runcommand-onend.sh /opt/retropie/configs/all/
-if [[ $? -ne 0 ]]; then
-   echo "Error: Could not copy onend script!"
-   exit 1
-fi
-cp ./runcommand-onstart.sh /opt/retropie/configs/all/
-if [[ $? -ne 0 ]]; then
-   echo "Error: Could not copy onstart script!"
-   exit 1
-fi
-cd ..
+    # Copy Runcommands
+    cp $TEMPDIR/opt-retropie-configs-all/runcommand-on{end,start}.sh /opt/retropie/configs/all/
 
+    # Copy configs
+    cp -Rf $TEMPDIR/opt-retropie-configs/* /opt/retropie/configs/
+    chown -R pi:pi /opt/retropie/configs
 
-cd opt-retropie-configs
-chown pi:pi -R ./*
-cp -R -f ./* /opt/retropie/configs/
-if [[ $? -ne 0 ]]; then
-   echo "Error: Could copy retroarch configs!"
-   exit 1
-fi
-cd ..
+    # Copy RetroTink theme
+    cp -Rf $TEMPDIR/tft-retrotink /etc/emulationstation/themes/
 
-chown pi:pi -R ./tft-retrotink
-cp -R -f ./tft-retrotink /etc/emulationstation/themes/
-if [[ $? -ne 0 ]]; then
-   echo "Error: Could copy ES Theme!"
-   exit 1
-fi
+    # Update Samba Shares
+    cat << EOF >> /etc/samba/smb.conf
+[SaveStates]
+comment = pi
+path = "/home/pi/RetroPie/savestates"
+writeable = yes
+guest ok = yes
+create mask = 0644
+directory mask = 0755
+force user = pi
+follow symlinks = yes
+wide links = yes
 
-#Update Samba Shares
-echo '[SaveStates]' >> /etc/samba/smb.conf
-echo 'comment = pi' >> /etc/samba/smb.conf
-echo 'path = "/home/pi/RetroPie/savestates"' >> /etc/samba/smb.conf
-echo 'writeable = yes' >> /etc/samba/smb.conf
-echo 'guest ok = yes' >> /etc/samba/smb.conf
-echo 'create mask = 0644' >> /etc/samba/smb.conf
-echo 'directory mask = 0755' >> /etc/samba/smb.conf
-echo 'force user = pi' >> /etc/samba/smb.conf
-echo 'follow symlinks = yes' >> /etc/samba/smb.conf
-echo 'wide links = yes' >> /etc/samba/smb.conf
-echo '[SaveFiles]' >> /etc/samba/smb.conf
-echo 'comment = pi' >> /etc/samba/smb.conf
-echo 'path = "/home/pi/RetroPie/savefiles"' >> /etc/samba/smb.conf
-echo 'writeable = yes' >> /etc/samba/smb.conf
-echo 'guest ok = yes' >> /etc/samba/smb.conf
-echo 'create mask = 0644' >> /etc/samba/smb.conf
-echo 'directory mask = 0755' >> /etc/samba/smb.conf
-echo 'force user = pi' >> /etc/samba/smb.conf
-echo 'follow symlinks = yes' >> /etc/samba/smb.conf
-echo 'wide links = yes' >> /etc/samba/smb.conf
+[SaveFiles]
+comment = pi
+path = "/home/pi/RetroPie/savefiles"
+writeable = yes
+guest ok = yes
+create mask = 0644
+directory mask = 0755
+force user = pi
+follow symlinks = yes
+wide links = yes
+EOF
 
-cp -f ./config.txt /boot/config.txt
-if [[ $? -ne 0 ]]; then
-   echo "Error: Could not copy CONFIG.TXT!"
-   exit 1
-fi
-CURRENTDIR=$PWD
-cd ..
-rm -R $CURRENTDIR
-$DIALOG --title " RetroTINK Installation Script" --clear \
-        --yesno "\n\n Installation Complete!  Please reboot your Rasbperry Pi now to use your new RetroTINK enabled RetroPie!\n\nReboot now?" 20 40
-case $? in
-  0)
-    sync;sync;reboot;;
-  1)
-    exit 0;;
-  255)
-    exit 0;;
-esac
+    cp /boot/config.txt{,.retrotink}
+    cp -f $TEMPDIR/config.txt /boot/config.txt
+    rm -rf $TEMPDIR
+    $DIALOG --title "RetroTINK Installation Script" --clear \
+            --yesno "\n\nInstallation Complete!  Please reboot your Rasbperry Pi now to use your new RetroTINK enabled RetroPie!\n\nReboot now?" 20 40
+    case "$?" in
+    0)
+        sync;sync;reboot
+        ;;
+    1|255)
+        exit 0
+        ;;
+    esac
 }
 
 Failed_Install (){
-   $DIALOG --title  "Install Failed!" --clear \
-   --msgbox "\n\nSee log for details..." 20 40
-   clear;exit 1
+    $DIALOG --title  "Install Failed!" --clear \
+    --msgbox "\n\nSee log for details..." 20 40
+    clear;exit 1
 }
 
 Stopped_Install (){
-   $DIALOG --title  "Install Canceled!" --clear \
-   --msgbox "\n\nExiting..." 20 40
-   clear;exit 1
-   
+    $DIALOG --title  "Install Canceled!" --clear \
+    --msgbox "\n\nExiting..." 20 40
+    clear;exit 1
 }
 
-# Script has Internet and Root access.  Good to go!
 Main_Program (){
-$DIALOG --title " RetroTINK Installation Script" --clear \
-        --yesno "\nThis program will install the needed files & modify some settings to enable the use of the RetroTINK Raspberry Pi HAT with RetroPie (versions 4.3 and above)\n\nWARNING: This script should be run on a fresh installation of RetroPie and may not function properly or at all if changes have been made.\n\nContinue installation?" 20 40
-case $? in
-  0)
-    Continue_Install;;
-  1)
-    Stopped_Install;;
-  255)
-    Stopped_Install;;
-esac
+    $DIALOG --title "RetroTINK Installation Script" --clear \
+            --yesno "\nThis program will install the needed files & modify some settings to enable the use of the RetroTINK Raspberry Pi HAT with RetroPie (versions 4.3 and above)\n\nWARNING: This script should be run on a fresh installation of RetroPie and may not function properly or at all if changes have been made.\n\nContinue installation?" 20 40
+    case "$?" in
+        0)
+            Continue_Install;;
+        1|255)
+            Stopped_Install;;
+    esac
 }
 
 Check_Root
-Check_Internet
+git clone https://github.com/marcteale/retroTINK-setup.git $TEMPDIR
 Main_Program
+rm -rf $TEMPDIR
